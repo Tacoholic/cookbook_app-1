f58d05d3e416a50421452524311010675ce77ae9
JackMarx <seriousfools@gmail.com>
2019-02-14 11:58:35 -0600

    dom and axios

--- app/views/api/recipes/show.json.jbuilder
+++ app/views/api/recipes/show.json.jbuilder
@@ -1,5 +1,1 @@
-if current_user
-  json.current_user current_user.email
-end
-
 json.partial! @recipe, partial: "recipe", as: :recipe
---
+++ public/index.js
@@ -0,0 +1,24 @@
+
+axios.get("http://localhost:3000/api/recipes")
+  .then(function(response) {
+    var divElement = document.querySelector('div');
+
+    var newContent = "";
+
+    response.data.forEach(function(recipe) {
+      newContent += '<h1>' + recipe.title + '</h1>'
+      newContent += '<h4>Prep Time: ' + recipe.formatted.prep_time + '</h4>'
+      newContent += '<h2>Ingredients: </h2>'
+      newContent += '<ul>'
+
+      recipe.formatted.ingredients.forEach(function(ingredient) {
+          newContent += '<li>' + ingredient + '</li>'
+      });
+
+      newContent += '</ul>'
+      newContent += '<br>'
+    });
+
+
+    divElement.innerHTML = newContent;
+  });
